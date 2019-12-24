2775586d474864f2283373cfcb089efff95e759c
Vic Wu <madcocomo@gmail.com>
2019-12-24 16:12:01 +0800

    Longest duplicate substring

--- src/main/java/io/xp/tdd/DuplicateSubStringFinder.java
+++ src/main/java/io/xp/tdd/DuplicateSubStringFinder.java
@@ -3,7 +3,9 @@ package io.xp.tdd;
 public class DuplicateSubStringFinder {
     public String longest(String str) {
         if (str.length() > 1) {
-            return str.substring(0, 1);
+            if (str.substring(0, 1).equals(str.substring(1, 2))) {
+                return str.substring(0, 1);
+            }
         }
         return "";
     }
