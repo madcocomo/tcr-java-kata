3eaef6d021acb92b74be641a0495ede7c81dcb4e
Vic Wu <madcocomo@gmail.com>
2019-12-24 16:05:08 +0800

    Longest duplicate substring

--- src/test/java/io/xp/tdd/DuplicateSubStringFinderTest.java
+++ src/test/java/io/xp/tdd/DuplicateSubStringFinderTest.java
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DuplicateSubStringFinderTest {
+    DuplicateSubStringFinder finder = new DuplicateSubStringFinder();
     @Test
     void get_empty_from_empty() {
-        DuplicateSubStringFinder finder = new DuplicateSubStringFinder();
         assertEquals("", finder.longest(""));
     }
 
     @Test
     void get_empty_from_single_character() {
-        DuplicateSubStringFinder finder = new DuplicateSubStringFinder();
         assertEquals("", finder.longest("a"));
     }
 }
