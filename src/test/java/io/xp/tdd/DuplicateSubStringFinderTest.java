eebe01e056ff7b1269a75862324219912399ae4d
Vic Wu <madcocomo@gmail.com>
2019-12-24 16:08:07 +0800

    Longest duplicate substring

--- src/test/java/io/xp/tdd/DuplicateSubStringFinderTest.java
+++ src/test/java/io/xp/tdd/DuplicateSubStringFinderTest.java
@@ -1,14 +1,24 @@
 package io.xp.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DuplicateSubStringFinderTest {
     DuplicateSubStringFinder finder = new DuplicateSubStringFinder();
-    @Test
-    void get_empty_from_empty() {
-        assertEquals("", finder.longest(""));
+    @ParameterizedTest
+    @MethodSource("stringAndExpect")
+    void get_empty_from_empty(String str, String expect) {
+        assertEquals(expect, finder.longest(str));
+    }
+
+    static Stream<Arguments> stringAndExpect() {
+        return Stream.of(Arguments.of("", ""));
     }
 
     @Test
