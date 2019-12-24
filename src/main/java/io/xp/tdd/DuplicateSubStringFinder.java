a173d6f4c8a243fb14375fdc51b61e9b01a81279
Vic Wu <madcocomo@gmail.com>
2019-12-24 16:21:06 +0800

    Longest duplicate substring

--- src/main/java/io/xp/tdd/DuplicateSubStringFinder.java
+++ src/main/java/io/xp/tdd/DuplicateSubStringFinder.java
@@ -9,9 +9,11 @@ public class DuplicateSubStringFinder {
 
     public String longest() {
         if (str.length() > 1) {
-            for (int j = 1; j < str.length(); j++) {
-                if (subString(0, 1).equals(subString(j, j+1))) {
-                    return subString(0, 1);
+            for (int i = 0; i < str.length(); i++) {
+                for (int j = i+1; j < str.length(); j++) {
+                    if (subString(i, i+1).equals(subString(j, j+1))) {
+                        return subString(i, i+1);
+                    }
                 }
             }
             for (int j = 1; j < str.length(); j++) {
