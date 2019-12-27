d5fa0e916fc17d2a0b8ff0cd372a0d392547c7a6
Vic Wu <madcocomo@gmail.com>
2019-12-27 14:43:02 +0800

    Wordwrap

--- src/main/java/io/xp/tdd/WordWrapper.java
+++ src/main/java/io/xp/tdd/WordWrapper.java
@@ -2,10 +2,10 @@ package io.xp.tdd;
 
 public class WordWrapper {
     public String wrap(String text, int lineWidth) {
-        if (text.length() > lineWidth) {
-            String remained = text.substring(lineWidth + 1);
-            return text.substring(0, lineWidth) + "\n" + wrap(remained, lineWidth);
+        if (text.length() <= lineWidth) {
+            return text;
         }
-        return text;
+        String remained = text.substring(lineWidth + 1);
+        return text.substring(0, lineWidth) + "\n" + wrap(remained, lineWidth);
     }
 }
