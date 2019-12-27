b0f042841cac4c61c2a523cc2ade9d8a65b8fc29
Vic Wu <madcocomo@gmail.com>
2019-12-27 14:32:55 +0800

    Wordwrap

--- src/test/java/io/xp/tdd/WordWrapperTest.java
+++ src/test/java/io/xp/tdd/WordWrapperTest.java
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordWrapperTest {
+    WordWrapper wrapper = new WordWrapper();
     @Test
     void keep_same_when_shorter_than_line_width() {
-        WordWrapper wrapper = new WordWrapper();
         assertEquals("apple", wrapper.wrap("apple", 10));
     }
 
     @Test
     void add_linebreak_right_after_word() {
-        WordWrapper wrapper = new WordWrapper();
         assertEquals("apple\ntree", wrapper.wrap("apple tree", 5));
     }
 }
