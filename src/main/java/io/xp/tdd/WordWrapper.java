2b82a35c5863c853dfb4dbedda8dbf73cfc6633c
Vic Wu <madcocomo@gmail.com>
2019-12-27 14:31:39 +0800

    Wordwrap

--- src/main/java/io/xp/tdd/WordWrapper.java
+++ src/main/java/io/xp/tdd/WordWrapper.java
@@ -2,6 +2,9 @@ package io.xp.tdd;
 
 public class WordWrapper {
     public String wrap(String text, int lineWidth) {
+        if (text.length() > lineWidth) {
+            return "apple\ntree";
+        }
         return text;
     }
 }
--- src/test/java/io/xp/tdd/WordWrapperTest.java
+++ src/test/java/io/xp/tdd/WordWrapperTest.java
@@ -9,6 +9,11 @@ public class WordWrapperTest {
     void keep_same_when_shorter_than_line_width() {
         WordWrapper wrapper = new WordWrapper();
         assertEquals("apple", wrapper.wrap("apple", 10));
+    }
 
+    @Test
+    void add_linebreak_right_after_word() {
+        WordWrapper wrapper = new WordWrapper();
+        assertEquals("apple\ntree", wrapper.wrap("apple tree", 5));
     }
 }
