5328b4305cef7410923e4609010a238ca9df5e24
Vic Wu <madcocomo@gmail.com>
2019-12-30 11:54:22 +0800

    Wordwrap

--- src/main/java/io/xp/tdd/WordWrapper.java
+++ src/main/java/io/xp/tdd/WordWrapper.java
@@ -9,22 +9,55 @@ public class WordWrapper {
         if (text.length() <= lineWidth) {
             return text;
         }
-        String remainder;
-        String line;
-        int breakIndex = text.lastIndexOf(LINEBREAK, lineWidth);
-        if (breakIndex >= 0) {
-            line = text.substring(0, breakIndex+1);
-            remainder = text.substring(breakIndex+1);
-            return line + format(remainder, lineWidth);
-        }
-        int index = text.lastIndexOf(SPACE, lineWidth);
-        if (index >= 0) {
-            remainder = text.substring(index+1);
-            line = text.substring(0, index);
-        } else {
-            remainder = text.substring(lineWidth);
-            line = text.substring(0, lineWidth);
-        }
+        LineAndRemainder lineAndRemainder = new LineAndRemainder(text, lineWidth).invoke();
+        if (lineAndRemainder.is()) return lineAndRemainder.getLine() + format(lineAndRemainder.getRemainder(), lineWidth);
+        String remainder = lineAndRemainder.getRemainder();
+        String line = lineAndRemainder.getLine();
         return  line + LINEBREAK + format(remainder, lineWidth);
+    }
+
+    private class LineAndRemainder {
+        private boolean myResult;
+        private String text;
+        private int lineWidth;
+        private String remainder;
+        private String line;
+
+        public LineAndRemainder(String text, int lineWidth) {
+            this.text = text;
+            this.lineWidth = lineWidth;
+        }
+
+        boolean is() {
+            return myResult;
+        }
+
+        public String getRemainder() {
+            return remainder;
+        }
+
+        public String getLine() {
+            return line;
+        }
+
+        public LineAndRemainder invoke() {
+            int breakIndex = text.lastIndexOf(LINEBREAK, lineWidth);
+            if (breakIndex >= 0) {
+                line = text.substring(0, breakIndex+1);
+                remainder = text.substring(breakIndex+1);
+                myResult = true;
+                return this;
+            }
+            int index = text.lastIndexOf(SPACE, lineWidth);
+            if (index >= 0) {
+                remainder = text.substring(index+1);
+                line = text.substring(0, index);
+            } else {
+                remainder = text.substring(lineWidth);
+                line = text.substring(0, lineWidth);
+            }
+            myResult = false;
+            return this;
+        }
     }
 }
