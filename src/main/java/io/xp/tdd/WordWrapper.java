80059b02737bfada19b0516107d1f03006fd702d
Vic Wu <madcocomo@gmail.com>
2019-12-30 11:19:05 +0800

    Wordwrap

---
+++ src/main/java/io/xp/tdd/WordWrapper.java
@@ -0,0 +1,7 @@
+package io.xp.tdd;
+
+public class WordWrapper {
+    public String format(String apple, int i) {
+        return apple;
+    }
+}
--- src/test/java/io/xp/tdd/WordWrapperTest.java
+++ src/test/java/io/xp/tdd/WordWrapperTest.java
@@ -1,4 +1,14 @@
 package io.xp.tdd;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WordWrapperTest {
+    @Test
+    void should_keep_same_if_shorter_than_line_width() {
+        WordWrapper wrapper = new WordWrapper();
+        assertEquals("apple", wrapper.format("apple", 10));
+
+    }
 }
